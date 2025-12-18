2b7839fc4144495746ec036d6ef12416e404d782
Ajavonstephan <ajavonstephan@gmail.com>
2025-12-18 16:37:44 +0100

    Premier commit

---
+++ Projet tmoney/autorisation.py
@@ -0,0 +1,6 @@
+Solde_compte = int(input("Entrer le solde de votre compte:"))
+Prix_Cadeau = int(input("Entrer le prix du cadeau:"))
+if Solde_compte >= Prix_Cadeau:
+    print("Vous pouvez acheter ce cadeau")
+else:
+    print("Vous ne pouvez pas acheter ce cadeau")
